45320d85c8e1e5607366f42c59ae08f0269897c0
Dgeko <dfhdfh13@gmail.com>
2022-12-10 13:27:28 +0300

    Добавил класс коробка, наследуется от сумки. добавил проверки условий различных методов в сумке

--- bag/workPlace.js
+++ bag/workPlace.js
@@ -1,9 +1,35 @@
+// создаем объект класса Bag
 let myBag = new Bag(100,100);
-console.log(myBag);
 
-let bottel = new Item('water',1,1);
-console.log(bottel);
+// создаем предметы класса Item
+let bottel = new Item('Water', 1, 1);
+let porridge = new Item("Rice", 2, 2.5);
+let iron = new Item('Iron', 99, 80);
+let ballon = new Item('Red_ballon', 1, 98);
 
+// добавляем предметы в сумку
 myBag.add_item(bottel);
+myBag.add_item(porridge);
+myBag.add_item(iron);
+myBag.add_item(ballon);
 
-console.log(...myBag.itemsAll);
+// показываем содержимое сумки
+myBag.show_itemsIn();
+
+// убираем бутылку из сумки
+myBag.delItemFrom('Water');
+
+// пытаемся добавить непредмет в сумку
+let numBB = 5;
+myBag.add_item(numBB);
+
+// создаем коробку
+let Ivan_box = new Box('Ivan', 150, 150);
+
+// добавляем предметы в коробку
+Ivan_box.add_item(bottel);
+Ivan_box.add_item(porridge);
+Ivan_box.add_item(iron);
+Ivan_box.add_item(ballon);
+//смотрим что в коробке
+Ivan_box.show_itemsIn();
